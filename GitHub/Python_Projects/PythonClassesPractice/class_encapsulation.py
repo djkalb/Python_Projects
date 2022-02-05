823a74dc89c2a41365e4be44e67fccdd113bb614
djkalb <71739290+djkalb@users.noreply.github.com>
2022-02-04 17:07:39 -0800

    added protected attribute to class encapsulation

--- GitHub/Python_Projects/PythonClassesPractice/class_encapsulation.py
+++ GitHub/Python_Projects/PythonClassesPractice/class_encapsulation.py
@@ -1,13 +1,15 @@
 from msilib.schema import Class
 
 class Private:
-    def __init__(self):
+    def __init__(self, protected):
+        self._protect = protected
         self.__secret = ""
     def getSecret(self):
         return self.__secret
     def setSecret(self, attr):
         self.__secret = attr
         return self.__secret
-priv = Private()
+priv = Private("ummm")
+print(priv._protect)
 priv.setSecret("shhh")
 print(priv.getSecret())
